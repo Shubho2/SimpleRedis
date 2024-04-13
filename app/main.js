0ca9297c3f169c892757aca73f315650e4bec0b3
Shubho Banerjee <shubho.banerjee@sap.com>
2024-04-13 16:58:40 +0530

    first phase

--- app/main.js
+++ app/main.js
@@ -1,11 +1,17 @@
-const net = require("net");
+// const http = require("http");
+
+const express = require("express");
+
+// Create an express app
+const app = express()
+app.use(express.json());
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-// const server = net.createServer((connection) => {
-//   // Handle connection
-// });
-//
-// server.listen(6379, "127.0.0.1");
+// const server = http.createServer((req, res) => {
+//    // Handle request
+// })
+
+app.listen(6379, "127.0.0.1");
