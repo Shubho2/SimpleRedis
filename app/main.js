4a0102c99e93fcdab6563f6c2ad132014cc86560
Shubho Banerjee <shubho.banerjee@sap.com>
2024-04-14 14:44:24 +0530

    5th stage

--- app/main.js
+++ app/main.js
@@ -2,8 +2,20 @@ const net = require("net");
 
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
-      console.log(data.toString());
-      connection.write("+PONG\r\n");
+        let args = data.toString().toLowerCase().split("\r\n");
+        console.log(args);
+        switch(args[2]){
+            case "echo":
+                console.log("Echoing");
+                connection.write(args[3] + "\r\n" + args[4] + "\r\n");
+                break;
+            case "ping":
+                console.log("Pinging");
+                connection.write("+PONG\r\n");
+                break;
+            default:
+                console.log("Command not found");
+        }
     });
     console.log("Client connected");
   });
