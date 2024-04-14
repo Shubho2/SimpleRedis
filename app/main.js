0be3da8ac7d1bb640523ccaa0a4d300c3ea81b85
Shubho Banerjee <shubho.banerjee@sap.com>
2024-04-14 10:08:47 +0530

    third stage complete

--- app/main.js
+++ app/main.js
@@ -1,18 +1,11 @@
-// const http = require("http");
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-
-
 const net = require("net");
 
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
       console.log(data.toString());
-    })
-  1
-    connection.write("+PONG\r\n");
+      connection.write("+PONG\r\n");
+    });
+
   });
 
 
